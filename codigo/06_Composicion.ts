2fa2b4ca6eac1edea4c700c9c1682b589b5a263a
AndDev <23031429@itcelaya.edu.mx>
2025-03-21 22:16:34 -0600

    Fix variable name from 'numeros' to 'listaNumeros' for consistency in 06_Composicion.ts

--- codigo/06_Composicion.ts
+++ codigo/06_Composicion.ts
@@ -7,21 +7,21 @@ const listaNumeros = [1, 2, 3, 4, 5];
 
 // Usando map para multiplicar cada número por 2
 
-const dobles = numeros.map((num) => num * 2);
+const dobles = listaNumeros.map((num) => num * 2);
 
 console.log(dobles); // [2, 4, 6, 8, 10]
 
 //* filter
 // El método `filter` crea un nuevo array con todos los elementos que pasen la prueba implementada por la función proporcionada.
 
-const mayoresQueTres = numeros.filter((num) => num > 3);
+const mayoresQueTres = listaNumeros.filter((num) => num > 3);
 
 console.log(mayoresQueTres); // [4, 5]
 
 //* reduce
 // El método `reduce` aplica una función a un acumulador y a cada valor de un array (de izquierda a derecha) para reducirlo a un único valor.
 
-const suma = numeros.reduce(
+const suma = listaNumeros.reduce(
   (acumulador, valorActual) => acumulador + valorActual,
   0
 );
@@ -31,7 +31,7 @@ console.log(suma); // 15
 //* forEach
 // El método `forEach` ejecuta la función proporcionada una vez por cada elemento del array.
 
-numeros.forEach(console.log);
+listaNumeros.forEach(console.log);
 // 1
 // 2
 // 3
@@ -42,13 +42,13 @@ numeros.forEach(console.log);
 
 //* Ejemplo 1: map y filter
 
-const resultado1 = numeros.map((num) => num * 2).filter((num) => num > 5);
+const resultado1 = listaNumeros.map((num) => num * 2).filter((num) => num > 5);
 
 console.log(resultado1); // [6, 8, 10]
 
 //* Ejemplo 2: map, filter y reduce
 
-const resultado2 = numeros
+const resultado2 = listaNumeros
   .map((num) => num * 2)
   .filter((num) => num > 5)
   .reduce((acumulador, valorActual) => acumulador + valorActual, 0);
@@ -59,7 +59,7 @@ console.log(resultado2); // 24 (6 + 8 + 10)
 
 const listaNombres = ["Ana", "Juan", "Pedro", "Maria"];
 
-nombres
+listaNombres
   .map((nombre) => nombre.toUpperCase())
   .forEach((nombre) => console.log(nombre));
 // ANA
